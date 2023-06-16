f874fa91a1d4a65f11c679da9e81b921c154a165
nfl <nawfalona@gmail.com>
2023-06-16 15:55:35 +0100

    si

--- pages/activities/index.vue
+++ pages/activities/index.vue
@@ -10,7 +10,7 @@
             <iframe
               src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fasso.akalntine&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
               width="500"
-              height="500"
+              height="700"
               frameborder="0"
               allowfullscreen="true"
               allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
