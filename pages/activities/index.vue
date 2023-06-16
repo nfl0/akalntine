c2c41ee6798bf9303a2a800f478b4b217e3ad9cf
nfl <nawfalona@gmail.com>
2023-06-16 16:01:04 +0100

    remove width,height params

--- pages/activities/index.vue
+++ pages/activities/index.vue
@@ -9,8 +9,6 @@
           <div class="iframe-wrapper">
             <iframe
               src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fasso.akalntine&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-              width="500"
-              height="700"
               frameborder="0"
               allowfullscreen="true"
               allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
