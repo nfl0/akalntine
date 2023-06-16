7809ee3a86b2c2231eef550fa95bc16195774283
nfl <nawfalona@gmail.com>
2023-06-16 02:39:30 +0100

    fix iframe x,y

--- pages/activities/index.vue
+++ pages/activities/index.vue
@@ -7,7 +7,7 @@
       <div class="row">
         <div class="col s12 m6">
           <div class="card activity-card">
-            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fasso.akalntine%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="100vh" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fasso.akalntine%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
           </div>
         </div>
       </div>
@@ -15,17 +15,14 @@
   </section>
 </template>
 
-<script>
-import axios from 'axios'
-const fm = require('front-matter')
-
-export default {
-  // ...
-}
-</script>
-
 <style lang="scss">
 .activities-page {
+  height: 100vh; /* Set the section height to fill the viewport */
+  display: flex; /* Use flexbox to align items */
+  flex-direction: column; /* Arrange items vertically */
+  justify-content: center; /* Center items vertically */
+  align-items: center; /* Center items horizontally */
+
   .activity-card {
     text-align: center;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
@@ -33,24 +30,17 @@ export default {
     border-radius: 8px;
     padding: 10px;
     max-width: 450px;
-    height: auto;
+    height: 100%; /* Make the card fill the section height */
   }
-  .activity-card:hover {
-    transform: scale(0.98, 0.99);
-    box-shadow: 0 0 10px rgb(88, 90, 90);
-    transition: 0.3s;
-  }
+
   .page-title {
     color: black;
     font: Bold 40px/43px Source Sans Pro;
   }
+
   .activities-wrapper {
     margin: 0 auto;
-  }
-  @media screen and (min-width: 600px) {
-    .activity-card {
-      height: 420px;
-    }
+    height: 100%; /* Make the wrapper fill the card height */
   }
 }
 </style>
